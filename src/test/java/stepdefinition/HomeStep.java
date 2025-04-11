e19adfbfe3417c372b29a54e4f994b201377992c
Bagavathi Samiappan <sbagavathi2006@gmail.com>
2025-04-11 17:34:33 -0400

    Created feature and step def for DSAlgoPortal and HOme features

--- src/test/java/stepdefinition/HomeStep.java
+++ src/test/java/stepdefinition/HomeStep.java
@@ -1,5 +1,73 @@
 package stepdefinition;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class HomeStep {
 
+	@Given("User is in home Page after clicking get started button in DS Algo page")
+	public void user_is_in_home_page_after_clicking_get_started_button_in_ds_algo_page() {
+
+	}
+
+	@When("User clicks NumpyNinja link")
+	public void user_clicks_numpy_ninja_link() {
+
+	}
+
+	@Then("Home page is refreshed")
+	public void home_page_is_refreshed() {
+
+	}
+
+	@When("User clicks Register link")
+	public void user_clicks_register_link() {
+
+	}
+
+	@Then("User is redirected to the Register page")
+	public void user_is_redirected_to_the_register_page() {
+
+	}
+
+	@When("User clicks Sign in link")
+	public void user_clicks_sign_in_link() {
+
+	}
+
+	@Then("User is redirected to Login page")
+	public void user_is_redirected_to_login_page() {
+
+	}
+
+	@When("User clicks the Data Structures dropdown")
+	public void user_clicks_the_data_structures_dropdown() {
+
+	}
+
+	@Then("User should sees six options Arrays, Linked List, Stack, Queue, Tree, Graph in dropdown menu")
+	public void user_should_sees_six_options_arrays_linked_list_stack_queue_tree_graph_in_dropdown_menu() {
+
+	}
+
+	@When("User selects {string} from the drop down")
+	public void user_selects_from_the_drop_down(String string) {
+
+	}
+
+	@Then("User views an alert {string} for the dropdown")
+	public void user_views_an_alert_for_the_dropdown(String string) {
+
+	}
+
+	@When("User clicks Get Started button on {string} flex")
+	public void user_clicks_get_started_button_on_flex(String string) {
+
+	}
+
+	@Then("User views an alert {string} for the flex")
+	public void user_views_an_alert_for_the_flex(String string) {
+
+	}
 }
