855501063c41143993f0b7cea063537f0eced893
Bagavathi Samiappan <sbagavathi2006@gmail.com>
2025-05-12 14:15:10 -0400

    Updated for cross browser and parallel testing

--- src/test/java/webdriver/DriverFactory.java
+++ src/test/java/webdriver/DriverFactory.java
@@ -1,42 +1,34 @@
 package webdriver;
-import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ThreadGuard;
 
-import utilities.LoggerLoad;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverFactory {
 	
 		public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>(); //each thread gets its own instance for parallel testing
-		   
-		public WebDriver init_driver(String browser) { //Initializes the WebDriver based on the given browser name
-			LoggerLoad.info("browser value is :" + browser);
-			
-			if(browser.equalsIgnoreCase("chrome")) {
-				 tlDriver.set(new ChromeDriver());//Assign ChromeDriver to the current thread's WebDriver
-			}
-			
-			else if(browser.equals("firefox")) {  
-				 tlDriver.set(new FirefoxDriver());
-			}
-				
-			else if(browser.equals("edge")) {
-				 tlDriver.set(new EdgeDriver());
-			}
-			
-			else {
-				LoggerLoad.error("Please pass the correct browser");
-			}
-			
-			getDriver().manage().deleteAllCookies();
-			getDriver().manage().window().maximize(); 
-			getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-			LoggerLoad.info("Driver inialized with browser : "+ browser);
-			return getDriver(); // Return the initialized driver
-		}
 		
+		public WebDriver init_driver(String browserName) {
+		    switch (browserName.toLowerCase()) {
+		        case "chrome":
+		            WebDriverManager.chromedriver().setup();
+		            tlDriver.set(ThreadGuard.protect(new ChromeDriver()));
+		            break;
+		        case "edge":
+		            WebDriverManager.edgedriver().setup();
+		            tlDriver.set(ThreadGuard.protect(new EdgeDriver()));
+		            break;
+		        default:
+		            throw new IllegalArgumentException("Browser not supported: " + browserName);
+		    }
+		    getDriver().manage().window().maximize();
+		    return getDriver();
+		}
+		   
 		public static WebDriver getDriver() { // Returns the WebDriver instance for the current thread
 			return tlDriver.get();
 		}
